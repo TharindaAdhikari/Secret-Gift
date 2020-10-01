302899ea88cae21f0ea97f4e1e06addd244da957
TTA <tharitash123@gmail.com>
2020-10-01 10:07:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Prevalent/Prevalent.java
@@ -0,0 +1,13 @@
+package com.example.myapplication.Prevalent;
+
+import com.example.myapplication.Model.Sellers;
+import com.example.myapplication.Model.Users;
+
+public class Prevalent {
+
+    public static Users currentOnlineUsers;
+    private static Sellers currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
