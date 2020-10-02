25fc763367d19991d259f48f374c7d6be417c6aa
Isuru boy <isurunirash@gmail.com>
2020-10-02 20:19:22 +0530

    update some things SellerLogin Activity

--- app/src/main/java/com/example/myapplication/SellerLoginActivity.java
+++ app/src/main/java/com/example/myapplication/SellerLoginActivity.java
@@ -1,5 +1,8 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,9 +11,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.Model.Sellers;
 import com.google.firebase.database.DataSnapshot;
@@ -51,15 +51,17 @@ public class SellerLoginActivity extends AppCompatActivity {
         String password = sInputPassword.getText().toString();
 
         if(TextUtils.isEmpty(name)){
+            sInputName.setError("Name Required");
             Toast.makeText(this,"Please Enter Your User Name",Toast.LENGTH_SHORT).show();
         }
         else if(TextUtils.isEmpty(password)){
+            sInputPassword.setError("Please Enter Strong Password");
             Toast.makeText(this,"Please Enter Your Password",Toast.LENGTH_SHORT).show();
         }
         else
         {
             loadingBar.setTitle("Login Account");
-            loadingBar.setMessage("Please wait, while we are checking the credentials");
+            loadingBar.setMessage("Please Wait, while We Are Checking The Credentials");
             loadingBar.setCanceledOnTouchOutside(false);
             loadingBar.show();
 
@@ -98,7 +100,7 @@ public class SellerLoginActivity extends AppCompatActivity {
                 }
                 else
                 {
-                    Toast.makeText(SellerLoginActivity.this, "Account with this" + name + "User Name Do Not Exists", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(SellerLoginActivity.this, "Account With This" + name + "User Name Do Not Exists", Toast.LENGTH_SHORT).show();
                     loadingBar.dismiss();
                 }
             }
